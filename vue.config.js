74f3eb9876a7f80a1734e3a8ab8cb53893977279
Telanium <medeirosmatheus50677@gmail.com>
2024-01-13 15:40:17 -0300

    try deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  productionSourceMap: false,
+  configureWebpack: {
+    performance: {
+      hints: process.env.NODE_ENV === 'production' ? 'warning' : false
+    }
+  }
 })
