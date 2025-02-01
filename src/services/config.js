6088b67951df5c744989a6466ff1d23deaee0979
Matheus_MB <115567535+Matheus-M-Braga@users.noreply.github.com>
2025-02-01 20:06:15 -0300

    feat: set api adress

--- src/services/config.js
+++ src/services/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://localhost:7126/api/",
+  baseURL: "https://locadoraapp-f5dyh7dthmgzajg5.brazilsouth-01.azurewebsites.net/api/",
 });
 
 export default api;
